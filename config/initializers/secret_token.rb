bad2850373e6540a9d3e350d2263619f9c1c7117
priyadarsini <priyadarsini@railsfactory.org>
2012-04-06 18:46:08 +0530

    Application added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApplicationTweet::Application.config.secret_token = '23dac8e47f4219a2d3127fb8bf1479486d7c8cc73225dbcbfefc2e36b92a911b01cf22a64f81ba2755bd378c6c86f0ebd8a3519e34a2676fa8f8131a8a059296'
